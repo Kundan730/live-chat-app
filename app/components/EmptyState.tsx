50aade7afd156f9e26ca4c337117ba3a6200c5c8
Kundan Kumar Sahu <espkundan@gmail.com>
2023-05-26 09:03:35 +0530

    added sidebar

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -11,7 +11,7 @@ const EmptyState = () => {
       flex
       justify-center
       items-center
-      bg-gray-100
+      bg-slate-200
 
     '>
       <div className="text-center items-center flex flex-col">
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,7 +1,14 @@
+import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
 async function Sidebar({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
       <main className="lg:pl-20 h-full">
+        <DesktopSidebar currentUser={currentUser!} />
+        <MobileFooter />
         {children}
       </main>
     </div>
