49d398078649c19917c856b3f500fa3ccde9e086
Kundan Kumar Sahu <espkundan@gmail.com>
2023-05-26 09:03:35 +0530

    added sidebar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//     swcPlugins: [["next-superjson-plugin", {}]]
+//   },
+//   images: {
+//     domains: [
+//       'res.cloudinary.com', 
+//       'avatars.githubusercontent.com',
+//       'lh3.googleusercontent.com'
+//     ]
+//   }
+// }
 
 module.exports = nextConfig
