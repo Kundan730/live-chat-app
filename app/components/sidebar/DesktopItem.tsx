0d283414bd6a496612c118f79d44131ee35a1130
Kundan Kumar Sahu <espkundan@gmail.com>
2023-05-31 05:27:57 +0530

    added conversations

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -41,7 +41,7 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
           hover:text-black
           hover:bg-gray-100
         `,
-          active && 'bg-gray-100 text-black')}
+          active && 'bg-gray-100 text-blue-700')}
         >
         <Icon className="h-6 w-6 shrink-0" />
         <span className='sr-only'>{label}</span>
