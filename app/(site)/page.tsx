8ee81dc7384f6f27201882090a4231d81063ac72
Kundan Kumar Sahu <espkundan@gmail.com>
2023-06-16 20:08:11 +0530

    basic change

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -25,6 +25,7 @@ export default function Home() {
         />
 
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Rapid-Chat-App:
           Sign in to your account
         </h2>
       </div>
