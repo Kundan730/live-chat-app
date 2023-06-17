63d3e0e149da06160b165eab1c2d41922cf6c338
Kundan Kumar Sahu <espkundan@gmail.com>
2023-06-17 06:53:33 +0530

    test6

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         />
 
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Rapid-Chat-App:
+          Rapid-Chat-App:-
           Sign in to your account
         </h2>
       </div>
